3973a0565bc03863d440b41f6292f59894e6ba7f
Simone Ballard <simone.a.ballard@gmail.com>
2021-07-01 17:52:15 -0600

    menu tags filled out

---
+++ lwb-music/src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Menu from '../components/Menu';
+
+function About() {
+    return (
+        <div>
+        <Menu 
+            oneTxt='Home'
+            oneLink='/home'
+            twoTxt='Music'
+            twoLink='/music'
+            threeTxt='Resources'
+            threeLink='/resources'
+            fourTxt='Contact'
+            fourLink='/contact'
+        />
+        </div>
+    )
+}
+
+export default About
--- lwb-music/src/pages/Home.js
+++ lwb-music/src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Menu from '../components/Menu';
 
 function Home() {
     return (
         <div>
-            
+        <Menu 
+            oneTxt='About'
+            oneLink='/about'
+            twoTxt='Music'
+            twoLink='/music'
+            threeTxt='Resources'
+            threeLink='/resources'
+            fourTxt='Contact'
+            fourLink='/contact'
+        />
         </div>
     )
 }
