ce3519d32ca934d462fc3104d84de8311dde91bd
Simone Ballard <simone.a.ballard@gmail.com>
2021-07-01 18:00:04 -0600

    routes set up in App

--- lwb-music/src/App.js
+++ lwb-music/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//components
+import Contact from './pages/Contact';
+import Resources from './pages/Resources';
+import Music from './pages/Music';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/resources' component={Resources} />
+        <Route exact path='/music' component={Music} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/' component={Home} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
