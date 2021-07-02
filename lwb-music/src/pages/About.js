bd0863a67ea691e9b8f72a18cde0ef1cb06895f6
Simone Ballard <simone.a.ballard@gmail.com>
2021-07-02 00:49:19 -0600

    more content added to about page

--- lwb-music/src/pages/About.js
+++ lwb-music/src/pages/About.js
@@ -21,7 +21,7 @@ function About() {
                 flic={School}
                 title='Humble Beginnings'
                 subtitle='"Kill the Indian in him and save the man"'
-                text='Louis attended the Seneca Indian Boarding School that was located in Wyandotte, Oklahoma from the age of six.'
+                text='Louis was born to Quapaw and Cherokee parents near Devils Promenade, Oklahoma on July 8, 1931. He attended the Seneca Indian Boarding School that was located in Wyandotte, Oklahoma from the age of six. Boys at the school were taught carpentry and farm management. The school was initially founded in the 1870s by Quakers as a mission. The general purpose of all Indian boarding schools was to remove children from their traditional customs and assimilate them into the predominant European culture. Louis resisted the strict indoctrination techniques and contiuned to speak his native Quapaw language and practice native dances. He was punished for doing so, but left the school retaining his culture and language.'
             />
         </div>
         </div>
